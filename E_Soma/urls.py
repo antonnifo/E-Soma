541197d68de9f7e88f37745a18a65616b909bb22
antonnifo <antonnifo@live.com>
2022-01-19 19:51:27 +0300

    register courses app

--- E_Soma/urls.py
+++ E_Soma/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    
+    path('', include('courses.urls', namespace='courses')),
     path('admin/', admin.site.urls),
 ]
 
