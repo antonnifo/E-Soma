f51a47ae9572bf4c5dd27a286f0834b0314b1c8d
antonnifo <antonnifo@live.com>
2022-01-26 12:30:15 +0300

    add URL patterns for the list, create, edit, and delete course views

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home
+from . import views
 
-app_name = 'courses'
 urlpatterns = [
-    path('' , home ,name='mtaa'),
+    path('mine/',views.ManageCourseListView.as_view(),name='manage_course_list'),
+    path('create/',views.CourseCreateView.as_view(), name='course_create'),
+    path('<pk>/edit/', views.CourseUpdateView.as_view(),name='course_edit'),         
+    path('<pk>/delete/',views.CourseDeleteView.as_view(), name='course_delete'),
 ]
