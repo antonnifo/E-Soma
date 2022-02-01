4e0492cf8761d310d12db60398ed89bb96b6192b
antonnifo <antonnifo@live.com>
2022-02-01 12:41:11 +0300

    Add field students to course model

--- courses/models.py
+++ courses/models.py
@@ -30,6 +30,9 @@ class Course(models.Model):
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
+    students = models.ManyToManyField(User, related_name='courses_joined',
+                                    blank=True)
+
     class Meta:
         ordering = ['-created']
 
