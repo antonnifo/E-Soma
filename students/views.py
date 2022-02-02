086850f4ee5e92445e2d72c3b1a2f2aa6a893427
antonnifo <antonnifo@live.com>
2022-02-02 13:46:10 +0300

    add views to list and see details of an enrolled course

--- students/views.py
+++ students/views.py
@@ -1,3 +1,6 @@
+from django.views.generic.detail import DetailView
+from courses.models import Course
+from django.views.generic.list import ListView
 from django.views.generic.edit import FormView
 from .forms import CourseEnrollForm
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -24,6 +27,7 @@ class StudentRegistrationView(CreateView):
 
         return result
 
+
 class StudentEnrollCourseView(LoginRequiredMixin, FormView):
     course = None
     form_class = CourseEnrollForm
@@ -35,3 +39,33 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
 
     def get_success_url(self):
         return reverse_lazy('student_course_detail', args=[self.course.id])
+
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # get course object
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            # get current module
+            context['module'] = course.modules.get(id=self.kwargs['module_id'])
+        else:
+            # get first module
+            context['module'] = course.modules.all()[0]
+        return context
