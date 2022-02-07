bae3e5bf2b71296dc9416c05ff3615d3731b5b48
antonnifo <antonnifo@live.com>
2022-02-07 12:44:21 +0300

    create a view that mimics the behavior of the retrieve() action, but includes the course contents

--- courses/api/views.py
+++ courses/api/views.py
@@ -7,7 +7,8 @@ from rest_framework.views import APIView
 from rest_framework.decorators import action
 
 from ..models import Course, Subject
-from .serializers import SubjectSerializer, CourseSerializer 
+from .serializers import SubjectSerializer, CourseSerializer, CourseWithContentsSerializer
+from .permissions import IsEnrolled
 
 
 class SubjectListView(generics.ListAPIView):
@@ -19,13 +20,19 @@ class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
+
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-
-    @action(detail=True,methods=['post'], authentication_classes=[BasicAuthentication], permission_classes=[IsAuthenticated])
+    @action(detail=True, methods=['post'], authentication_classes=[BasicAuthentication], permission_classes=[IsAuthenticated])
     def enroll(self, request, *args, **kwargs):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(detail=True, methods=['get'], serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
