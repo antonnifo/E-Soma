cde44164b02f096138146e3246d5b766ffe7ddf8
antonnifo <antonnifo@live.com>
2022-02-07 12:21:24 +0300

    enroll students via CourseViewSer

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
 
 app_name = 'courses'
 
@@ -7,7 +11,6 @@ urlpatterns = [
                 path('subjects/', views.SubjectListView.as_view(),
                         name='subject_list'),
                 path('subjects/<pk>/', views.SubjectDetailView.as_view(),
-                        name='subject_detail'),
-                path('courses/<pk>/enroll/', views.CourseEnrollView.as_view(),
-                        name='course_enroll'),        
+                        name='subject_detail'),    
+                path('', include(router.urls)),           
 ]
