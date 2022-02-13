6d9075790fcb075baee5b511046dca757e257fc0
antonnifo <antonnifo@live.com>
2022-02-13 12:12:37 +0300

    add as_asgi() for consumer class

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-            re_path(r'ws/chat/room/(?P<course_id>\d+)/$', consumers.ChatConsumer),
+            re_path(r'ws/chat/room/(?P<course_id>\d+)/$', consumers.ChatConsumer.as_asgi()),
 ]
